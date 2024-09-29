50f6b40d953a7dba9b66e043c5a7af3db048a51c
Karunesh04 <karuneshtripathi2004@gmail.com>
2024-09-29 18:56:02 +0530

    updated skills & projects page

---
+++ components/ProjectCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="shadow-md rounded-lg overflow-hidden flex flex-col justify-between">
+    <img :src="image" :alt="title" class="w-full h-48 object-cover"/>
+    <div class="p-4">
+      <h3 class="text-2xl sm:text-3xl font-semibold text-[#A97A13] mb-4">{{ title }}</h3>
+      <p class="text-gray-500 text-justify mb-4">{{ description }}</p>
+      <div class="flex justify-around items-center">
+        <a :href="demoLink" target="_blank" class="p-2 hover:text-[#A97A13] icon"><img class="w-8" src="/static/images/website-click-svgrepo-com.svg" alt="git"></a>
+        <a :href="codeLink" target="_blank" class="p-2 hover:text-[#A97A13] icon"><img class="w-8" src="/static/images/github-code-source-svgrepo-com.svg" alt="git"></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    demoLink: {
+      type: String,
+      required: true
+    },
+    codeLink: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
---
+++ components/SkillItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col items-center">
+    <img :src="src" :alt="alt" class="w-12 lg:w-16 icon" />
+    <p class="mt-2 text-lg font-semibold">{{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: String,
+    alt: String,
+    name: String,
+  },
+};
+</script>
+
+
